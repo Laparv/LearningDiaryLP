76a48484c41e3d4fba422ee4e9a069c85fa753a0
Laparv <lastappar@gmail.com>
2022-06-16 16:46:34 +0300

    added search functions working by id and title

--- LearningDiaryLP/Program.cs
+++ LearningDiaryLP/Program.cs
@@ -53,17 +53,70 @@ namespace LearningDiaryLP
                 //read previous entry from diary
                 else if (menuPick == "2")
                 {
-                    Console.WriteLine("Write entry ID to print it out");
-                    int userIdInput = Convert.ToInt32(Console.ReadLine());
-
-
-                    if (listOfTopics.ContainsKey(userIdInput))
+                    Console.WriteLine("1) Search by ID\n2)Search by Title");
+                    switch (Console.ReadLine())
                     {
-                        Console.WriteLine(listOfTopics[userIdInput].CompileString());
-                        Console.ReadLine();
+                        case "1":
+
+                            Console.WriteLine("Write entry ID to print it out");
+                            int userIdInput = Convert.ToInt32(Console.ReadLine());
+
+
+                            if (listOfTopics.ContainsKey(userIdInput))
+                            {
+                                Console.WriteLine(listOfTopics[userIdInput].CompileString());
+                                Console.ReadLine();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Nothing found with that ID");
+                                Console.ReadLine();
+
+                            }
+                            continue;
+
+                        case "2": 
+                            Console.WriteLine("Search with title:");
+                            string userTitleInput = Console.ReadLine();
+                            int readerKey = listOfTopics.FirstOrDefault(x => x.Value.Title == userTitleInput).Key;
+
+                            if (listOfTopics.ContainsKey(readerKey))
+                            {
+                                Console.WriteLine(listOfTopics[readerKey].CompileString());
+                                Console.ReadLine();
+                            }
+                            
+                            
+                            /*var result = listOfTopics.Where(topic => topic.Value.Title.Contains(userTitleInput));
+
+
+                            result = from topic in listOfTopics
+                                     where topic.Value.Title.Contains(userTitleInput)
+                                     select topic;
+
+                            foreach (var topic in result)
+                            {
+                                Console.WriteLine(topic.ToString());
+                            }*/
+
+
+                            //var readEntry = listOfTopics.Where(topic => topic.Value.Title.Contains(userTitleInput));
+
+
+
+
+                            //listOfTopics.Where(topic => topic.Value.Title.Contains(userTitleInput));
+
+                            //IEnumerable<Topic> result = (IEnumerable<Topic>)listOfTopics.Where(topic => topic.Value.Title.Contains(userTitleInput));
+
+
+                            Console.ReadLine();
+
+                            continue;
+
+                        default: continue;
                     }
-                    Console.ReadLine();
-                    continue;
+                    
                 }
                 //exit app
                 else if (menuPick == "3")
